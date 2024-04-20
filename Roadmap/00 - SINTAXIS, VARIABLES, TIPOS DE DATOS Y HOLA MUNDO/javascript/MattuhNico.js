f7f479077ed90c760624534bcf980a56efd149ec
MattuhNico <matiasnico.lopez@gmail.com>
2024-04-20 20:54:59 -0300

    Roadmap/#00/javascript/MattuhNico.js

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/javascript/MattuhNico.js
@@ -0,0 +1,55 @@
+// 1) Crea un comentario en el código y coloca la URL del sitio web oficial del lenguaje de programación que has seleccionado.
+
+// https://developer.mozilla.org/es/docs/Web/JavaScript
+
+// 2) Representa las diferentes sintaxis que existen de crear comentarios en el lenguaje (en una línea, varias...).
+
+// Comentario 1 sola línea
+
+/*
+    Comentario varias lineas
+*/
+
+// 3) Crea una variable (y una constante si el lenguaje lo soporta).
+
+// Variable global
+var variableGlobal = "Variable";
+// Variable local
+let variableLocal = "Variable";
+
+// Constante
+const constante = "Constante";
+
+// 4) Crea variables representando todos los tipos de datos primitivos del lenguaje (cadenas de texto, enteros, booleanos...).
+
+// String
+let cadena = "Cadena de texto";
+let otraCadena = 'Cadena de texto';
+// Numero entero
+let numeroEntero = 1;
+// Numero decimal 
+let numeroDecimal = 1.2;
+// Boolean (true - false)
+let booleanVerdadero = true;
+let booleanFalso = true;
+// Valor nulo
+let nulo = null;
+// Valor indefinido
+let indefinido = undefined;
+// Valor enorme
+let bigInt = 12132123131321321321321231321n;
+
+// 5) Imprime por terminal el texto: "¡Hola, [y el nombre de tu lenguaje]!"
+
+console.log("¡Hola, JavaScript!");
+
+// Extra: tipo de datos de las variables
+console.log(typeof cadena);
+console.log(typeof otraCadena);
+console.log(typeof numeroEntero);
+console.log(typeof numeroDecimal);
+console.log(typeof booleanVerdadero);
+console.log(typeof booleanFalso);
+console.log(typeof nulo);
+console.log(typeof indefinido);
+console.log(typeof bigInt);
